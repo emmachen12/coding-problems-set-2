19dc57d73da9d11dfa5089d89990425a24477730
emmachen12 <emc1207pi@gmail.com>
2024-02-09 14:46:43 -0500

    hey

---
+++ example3.java
@@ -0,0 +1,13 @@
+public class example3{
+    public static void main (String[] args)
+    {
+    System.out.println("Anderson\t3453");
+    System.out.println("Boyton\t\t5344");
+    System.out.println("Carrington\t3677");
+    System.out.println("Houston\t\t5988");
+    System.out.println("McDougal\t5233");
+    System.out.println("Robertson\t3623");
+    System.out.println("Stryker\t\t5112");
+
+    }
+}
